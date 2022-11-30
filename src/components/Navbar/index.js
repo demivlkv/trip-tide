ebb973acb3546ce616eebc8a8714244384f1cec7
Demi Hayashi <demi.h@me.com>
2022-11-30 13:04:13 -0800

    add useState in Navbar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { MagnifyingGlassIcon, UserCircleIcon, Bars2Icon } from "@heroicons/react/24/solid";
 import { Facebook, Instagram, Twitter, Youtube } from 'react-feather';
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const handleNav = () => setNav(!nav);
+
 	return (
-		<nav id="navbar">
+		<nav id="navbar" className={nav ? 'bg-[#f8f8f8de] transition-all ease-in-out duration-700' : ''}>
 			<div className="w-full h-[80px] px-4 flex justify-between items-center z-[5]">
 				<div className="logo">
 					<h2 className="text-gray-800 font-semibold uppercase">
@@ -26,11 +29,11 @@ const Navbar = () => {
 				</div>
 
 				{/* HAMBURGER MENU */}
-				<div className="hamburger-menu md:hidden">
+				<div className="hamburger-menu md:hidden hover:cursor-pointer" onClick={handleNav}>
 					<Bars2Icon width={30} />
 				</div>
 
-                <div className="mobile-menu w-full absolute top-[80px] left-0 flex flex-col justify-between bg-[#f8f8f8de]">
+                <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
 					<ul className="my-4">
 						<li className="py-5 px-4 border-b border-gray-200">Home</li>
 						<li className="py-5 px-4 border-b border-gray-200">Book</li>
