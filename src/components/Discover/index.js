3fd388395bf4a6b44283e25b4adb1bfb02f0cc35
Demi Hayashi <demi.h@me.com>
2022-12-01 09:49:58 -0800

    update Discover interface

--- src/components/Discover/index.js
+++ src/components/Discover/index.js
@@ -14,22 +14,70 @@ const Discover = () => {
             <h1 className="mb-8 text-2xl md:text-4xl font-semibold uppercase tracking-widest">
                 Discover dreamy destinations
             </h1>
-            <h2 className="mb-8 text-xl md:text-2xl font-light uppercase tracking-widest">
-                Don't miss these deals
-            </h2>
 
-            <div className="w-full md:max-w-screen-lg flex flex-row flex-wrap justify-center items-center">
-                <div className="left w-full md:w-[600px] pr-4">
-                    <div className="images grid grid-cols-2 gap-2 md:gap-4">
+            <div className="w-full md:max-w-screen-lg mx-auot flex flex-row flex-wrap justify-center items-center">
+                <div className="w-full md:max-w-screen-md">
+                    <form className="search-form w-full mx-auto grid grid-cols-1 md:grid-cols-3 mb-4">
+                        <div className="col-span-1 md:col-span-3">
+                            <input type="radio" id="round-trip" name="round-trip" value="round-trip" />
+                            <label for="round-trip" className="pl-2 pr-6">Round-Trip</label>
+                            <input type="radio" id="one-way" name="one-way" value="one-way" />
+                            <label for="one-way" className="pl-2">One-Way</label>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-3 col-span-1 md:col-span-3 gap-4 py-4 place-items-stretch">
+                            <div>
+                                <label className="block">From</label>
+                                <input type="text" placeholder="Origin city or airport" />
+                            </div>
+                            <div>
+                                <label className="block">To</label>
+                                <input type="text" placeholder="Destination city or airport" />
+                            </div>
+                            <div>
+                                <label className="block">Passengers</label>
+                                <select>
+                                    <option value="1">1 Passenger</option>
+                                    <option value="1">2 Passengers</option>
+                                    <option value="1">3 Passengers</option>
+                                    <option value="1">4 Passengers</option>
+                                    <option value="1">5 Passengers</option>
+                                    <option value="1">6 Passengers</option>
+                                    <option value="1">7 Passengers</option>
+                                    <option value="1">8 Passengers</option>
+                                    <option value="1">9 Passengers</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-3 col-span-1 md:col-span-3 gap-4 pb-4 place-items-stretch">
+                            <div>
+                                <label className="block">Departure Date</label>
+                                <input type="date" />
+                            </div>
+                            <div>
+                                <label className="block">Return Date</label>
+                                <input type="date" />
+                            </div>
+                            <div className="pt-6">
+                                <button className="w-full h-[43px]">Find Your Trip</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <h2 className="mb-8 text-teal-400 text-xl md:text-2xl font-light uppercase tracking-widest">
+                    Don't miss these deals
+                </h2>
+
+                <div className="images grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-4">
                         {/* <img
                             src={Italy}
                             alt="Soak in the view of the Almafi Coast, Italy"
                             className="col-span-2"
                         /> */}
-                        <img
+                        {/* <img
                             src={Kyoto}
                             alt="Get lost in the bamboo forest of Kyoto, Japan"
-                        />
+                        /> */}
                         <img
                             src={Iceland}
                             alt="Collect moments in Iceland"
@@ -39,27 +87,6 @@ const Discover = () => {
                             src={Mykonos}
                             alt="Wake up to a different world in Mykonos, Greece"
                         />
-                    </div>
-                </div>
-
-                <div className="right w-full md:w-[375px]">
-                    <form className="w-[80%] sm:w-full mx-auto">
-                        <div className="py-4">
-                            <label className="block">Destination</label>
-                            <input type="text" className="w-full" />
-                        </div>
-                        <div className="w-full flex justify-between items-center pb-4">
-                            <div>
-                                <label className="block">Departure Date</label>
-                                <input type="date" />
-                            </div>
-                            <div>
-                                <label className="block">Return Date</label>
-                                <input type="date" />
-                            </div>
-                        </div>
-                        <button className="w-full">Find Your Trip</button>
-                    </form>
                 </div>
             </div>
         </div>
