460e92a5edbe2cd722049d2dd55888097eacddc8
Demi Hayashi <demi.h@me.com>
2022-12-28 16:38:24 -0800

    update About.js

--- client/src/components/Home/About.js
+++ client/src/components/Home/About.js
@@ -2,20 +2,25 @@ import React from 'react';
 
 const About = () => {
 	return (
-		<div name="about" className="about w-full h-full md:h-screen">
-			<div className="relative w-full md:max-w-screen-lg h-full mx-auto p-8 flex flex-col justify-center items-center z-[2]">
+		<div name="about" className="about bg-about bg-cover bg-top w-full h-full md:h-screen">
+			<div className="w-full md:max-w-screen-lg mx-auto p-8 flex flex-col justify-center z-[2]">
 				<h1>
           Bringing the world to you
 				</h1>
-				<p className="pt-4">
+				<p className="my-4 text-center">
 					Trip Tide was founded in 2022 by a traveler, <em className="text-teal-400">for travelers</em>.
 				</p>
-				<p className="py-8">
-					We are a humble, small-scale travel agency to help plan your
-					next getaway. Our core belief is that everyone should travel, and it
-					is vital that we make travel accessible to everyone.
-				</p>
-				<div className="images grid grid-cols-2 md:grid-cols-5 gap-2 md:gap-4">
+				<div className="flex flex-wrap justify-evenly items-start bg-[#ffffffcc] rounded-lg leading-loose">
+					<div className="w-full md:w-1/2 p-4">
+						We are a humble, small-scale travel agency to help plan your
+						next getaway. Our core belief is that everyone should travel, and it
+						is vital that we make travel accessible to everyone.
+					</div>
+					<div className="w-full md:w-1/2 p-4">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					</div>
+				</div>
+				{/* <div className="images grid grid-cols-2 md:grid-cols-5 gap-2 md:gap-4">
 					<img
 						src="https://images.unsplash.com/photo-1633321088355-d0f81134ca3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
 						alt="Soak in the view of the Almafi Coast, Italy"
@@ -37,7 +42,7 @@ const About = () => {
 						src="https://images.unsplash.com/photo-1542082873-c1d89ae3a6ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
 						alt="Wake up to a different world in Hawaii"
 					/>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       backgroundImage: {
         'signup': 'url("https://images.unsplash.com/photo-1551361997-b3147f1942ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")',
         'login': 'url("https://images.unsplash.com/photo-1509650926597-25eead3b6ca9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")',
+        'about': 'url("https://images.unsplash.com/photo-1529410804717-979530809e26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=969&q=80")',
       }
     },
   },
