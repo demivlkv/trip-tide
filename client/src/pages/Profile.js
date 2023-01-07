bacdce112fbca99808c1f69464ce09e3081e0c6b
Demi Hayashi <demi.h@me.com>
2023-01-07 10:13:18 -0800

    update follow button functionality in profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate, Link, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
-import { UserPlus } from 'react-feather';
+import { UserPlus, Check } from 'react-feather';
 
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import { ADD_FRIEND } from '../utils/mutations';
@@ -10,11 +10,9 @@ import Layout from '../components/Layout/Dashboard';
 import UserWidget from '../components/UserWidget';
 import PostList from '../components/PostList';
 import PostForm from '../components/PostForm';
-import DeleteButton from '../components/DeleteButton';
 
 const Profile = () => {
   // retrieve user information
-  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
@@ -22,11 +20,25 @@ const Profile = () => {
   const user = data?.me || data?.user || {};
   const posts = user?.posts || [];
 
+  // follow users feature
+  const [addFriend] = useMutation(ADD_FRIEND);
+  const [followed, setFollowed] = useState(false);
+
+  // maintain `following` state for follow button
+  // useEffect(() => {
+  //   if (userParam && user.friends.includes((friend) => friend._id === user._id)) {
+  //     setFollowed(true);
+  //   } else {
+  //     setFollowed(false);
+  //   }
+  // }, [userParam, user]);
+
   const handleClick = async () => {
     try {
       await addFriend({
         variables: { id: user._id }
       });
+      setFollowed(true);
     } catch (e) {
       console.error(e);
     }
@@ -59,7 +71,7 @@ const Profile = () => {
             {/* IF USER IS LOGGED IN, DISPLAY POST FORM */}
             {!userParam && <PostForm />}
             {/* DISPLAY USER'S POSTS */}
-            {posts?.map(post => (
+            {user.username && posts?.map(post => (
               <div key={post._id}>
                 <PostList post={post} />
               </div>
@@ -76,11 +88,19 @@ const Profile = () => {
             />
             {userParam && (
               <div className="w-full flex justify-start pb-4 px-4">
-                <button onClick={handleClick} className="btn">
-                  <div className="w-full h-full inline-flex items-center font-normal">
-                    <UserPlus width={13} className="mr-1" /> Follow
-                  </div>
-                </button>
+                {followed ? (
+                  <button disabled className="btn">
+                    <div className="w-full h-full inline-flex items-center font-normal">
+                      <Check width={13} className="mr-1" /> Following
+                    </div>
+                  </button>
+                ) : (
+                  <button onClick={handleClick} className="btn">
+                    <div className="w-full h-full inline-flex items-center font-normal">
+                      <UserPlus width={13} className="mr-1" /> Follow
+                    </div>
+                  </button>
+                )}
               </div>
             )}
           </div>
